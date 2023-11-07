bd8e4bca505feaca6dc11ee07b048811bec1c8ee
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-11-07 14:53:37 +0000

    Add basic command line interface

--- src/gamma/mod.rs
+++ src/gamma/mod.rs
@@ -2,3 +2,10 @@ mod common;
 mod elaborator;
 mod parser;
 mod stager;
+
+pub fn run(line: &str) {
+	let ("", term) = parser::parse_dynamic_eof(&line).unwrap() else {
+		panic!("parsing completed before end of input")
+	};
+	println!("parsed: {:?}", term);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,61 @@ mod gamma;
 mod utility;
 
 mod parse;
+use std::str::FromStr;
+
+use bpaf::{choice, construct, short, Parser};
 use parse::*;
 
-fn main() {
-	let mut line = String::new();
-	std::io::stdin().read_line(&mut line).unwrap();
+enum LanguageOption {
+	Alpha,
+	Beta,
+	Gamma,
+}
 
-	beta::run(&line);
+impl FromStr for LanguageOption {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		use LanguageOption::*;
+		match s {
+			"a" => Ok(Alpha),
+			"b" => Ok(Beta),
+			"c" => Ok(Gamma),
+			_ => Err("no such language".to_owned()),
+		}
+	}
+}
+
+enum InputOption {
+	Direct(String),
+	FilePath(String),
+}
+
+struct Options {
+	language: LanguageOption,
+	input: InputOption,
+}
+
+fn main() {
+	let language = short('l').argument::<LanguageOption>("LANGUAGE");
+	let options: Options = construct!(Options {
+		language,
+		input(construct!([
+			c(short('c').argument::<String>("COMMAND").map(InputOption::Direct)),
+			f(short('f').argument::<String>("FILE").map(InputOption::FilePath)),
+		]))
+	})
+	.to_options()
+	.run();
+
+	let input = match options.input {
+		InputOption::Direct(command) => command,
+		InputOption::FilePath(file_path) => std::fs::read_to_string(file_path).unwrap(),
+	};
+
+	match options.language {
+		LanguageOption::Alpha => alpha::run(&input),
+		LanguageOption::Beta => beta::run(&input),
+		LanguageOption::Gamma => gamma::run(&input),
+	}
 }
