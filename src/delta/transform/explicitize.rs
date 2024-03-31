2f6ce505089da71d9db160745966b9197112ee5f
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2024-03-31 22:15:46 +0100

    Remove unused files

--- src/delta/transform/explicitize.rs
+++
@@ -1,7 +0,0 @@
-/// Inserts type computations and annotates scope-ending terminators with destructor calls.
-// Temporarily supress warnings.
-// FIXME: Remove this.
-#[allow(unused)]
-pub fn explicitize() {
-	unimplemented!();
-}
